9f2508fb766ea26b46e2ae81b79f35c9afee5dca
joaoFran829 <aluno21876@ipt.pt>
2023-07-05 20:22:42 +0100

    Rating API

---
+++ Projeto/Projeto/Projeto/Controllers/API/RatingAPIController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+using Projeto.Models;
+using Projeto.Areas.Identity.Data;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Identity;
+
+namespace Projeto.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RatingAPIController : Controller
+    {
+
+        private readonly ApplicationDBContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public RatingAPIController(ApplicationDBContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost("Avaliar")]
+        public async Task<ActionResult> Avaliar([FromForm] RatingTransportModel ratingAux)
+        {
+            var rating = new Rating();
+
+            rating.Stars = ratingAux.Stars;
+            rating.Establishment = ratingAux.Establishment;
+            rating.User = ratingAux.User;
+
+            var user = rating.User;
+            var establishment = rating.Establishment;
+
+            user.ListRatings.Add(rating);
+            establishment.ListRatings.Add(rating);
+
+
+            _context.Add(rating);
+            await _context.SaveChangesAsync();
+
+            return Ok(rating);
+
+        }
+    }
+}
---
+++ Projeto/Projeto/Projeto/Models/RatingTransportModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projeto.Models
+{
+    public class RatingTransportModel
+    {
+
+        
+        public int Id { get; set; }
+
+        
+        public int Stars { get; set; }
+
+        
+        public int UserFK { get; set; }
+        public User? User { get; set; } 
+
+        
+        public int EstablishmentFK { get; set; }
+        public Establishment? Establishment { get; set; } 
+
+    }
+}

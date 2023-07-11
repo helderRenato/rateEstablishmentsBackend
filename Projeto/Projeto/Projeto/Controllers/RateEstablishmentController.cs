6a497dddfb4e3ed20b550eda35981ea53d10faae
Hélder Renato <helder_renatoribeiro@hotmail.com>
2023-07-11 19:18:58 +0100

    Update RateEstablishmentController.cs

--- Projeto/Projeto/Projeto/Controllers/RateEstablishmentController.cs
+++ Projeto/Projeto/Projeto/Controllers/RateEstablishmentController.cs
@@ -37,7 +37,7 @@ namespace Projeto.Controllers
             ViewData["Establishments"] = new SelectList(_context.Establishment, "Id", "Name");
 
             var rating = _context.Rating
-                .Where(r => r.UserFK == establishmentRate.UserFK && r.EstablishmentFK == rating.EstablishmentFK)
+                .Where(r => r.UserFK == establishmentRate.UserFK && r.EstablishmentFK == establishmentRate.EstablishmentFK)
                 .FirstOrDefault(); 
                 
             if (ModelState.IsValid)
